c22d13301cabc4efe416c6629d1bd8a86aaa9060
bbaker2ud <151549970+bbaker2ud@users.noreply.github.com>
2023-12-12 15:37:16 -0500

    Update get_falcon_sensor.sh

--- get_falcon_sensor.sh
+++ get_falcon_sensor.sh
@@ -7,4 +7,6 @@ echo "Downloading Falcon Sensor..." &&
 gdown 1YnvSQmCgUE0lRs5Fauvfub_KsUhcnbCw &&
 echo "Installing Falcon Sensor..." &&
 dpkg --install falcon-sensor_6.38.0-13501_amd64.deb &&
-rm falcon-sensor_6.38.0-13501_amd64.deb
+rm falcon-sensor_6.38.0-13501_amd64.deb &&
+/opt/CrowdStrike/falconctl -s --cid=0FA34C2A8A4545FC9D85E072AFBABA4A-E7 &&
+systemctl start falcon-sensor
